c8c79812a61be8319b2b8121eabf5ed60f276bef
rebeccaang <rang5@illinois.edu>
2019-12-03 12:47:42 -0600

    I created the calendar.

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.finalproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +17,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button newTask = findViewById(R.id.newTask);
+        final CalendarView calendar = findViewById(R.id.calendarView);
 
         newTask.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NewtaskActivity.class);
+                final Intent intent = new Intent(MainActivity.this, NewtaskActivity.class);
+
+                //gets selected date and adds that information to the intent
+                calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+                    @Override
+                    public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                        intent.putExtra("year", i);
+                        intent.putExtra("month", i1);
+                        intent.putExtra("day", i2);
+                        Log.e("hi", i1 + " / " + i2 + " / " + i);
+                    }
+                });
+
                 startActivity(intent);
             }
         });
